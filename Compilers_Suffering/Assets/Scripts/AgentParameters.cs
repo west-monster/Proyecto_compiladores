634b8c91f89330b90ad71715b6efd776fe07f0bc
David <49421810+west-monster@users.noreply.github.com>
2022-11-14 14:39:20 -0500

    Lsystems beta

---
+++ Compilers_Suffering/Assets/Scripts/AgentParameters.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentParameters : MonoBehaviour
+{
+    public Vector3 position, direction;
+    public int length; 
+}
+
---
+++ Compilers_Suffering/Assets/Scripts/Rules/Rule.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ProceduralDunge/Rule")]
+public class Rule : ScriptableObject
+{
+    public string letter;
+    [SerializeField]
+    private string[] results = null;
+
+    public string GetResult()
+    {
+        return results[0];
+    }
+}
